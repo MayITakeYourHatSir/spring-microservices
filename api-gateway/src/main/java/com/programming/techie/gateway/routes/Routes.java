e90ab227a196ae77dad7b4137fc4228299936d89
tkb0005053 <tkb5053ivan@gmail.com>
2025-12-02 13:59:15 +0800

    feat: 新增各微服務 swagger 設定，並由 api-gateway 統一路由

--- api-gateway/src/main/java/com/programming/techie/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/programming/techie/gateway/routes/Routes.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.rewritePath;
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
@@ -18,6 +19,16 @@ public class Routes {
         return GatewayRouterFunctions.route("product-service")
                 .route(RequestPredicates.path("/api/products/**"), http())
                 .before(uri("http://localhost:8080"))
+
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("product-service-swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), http())
+                .before(uri("http://localhost:8080"))
+                .before(rewritePath("/aggregate/product-service", ""))
                 .build();
     }
 
@@ -30,10 +41,28 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("order-service-swagger")
+                .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), http())
+                .before(uri("http://localhost:8081"))
+                .before(rewritePath("/aggregate/order-service", ""))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> inventoryServiceRoute(){
         return GatewayRouterFunctions.route("inventory-service")
                 .route(RequestPredicates.path("/api/inventory/**"), http())
                 .before(uri("http://localhost:8082"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("inventory-service-swagger")
+                .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), http())
+                .before(uri("http://localhost:8082"))
+                .before(rewritePath("/aggregate/inventory-service", ""))
                 .build();
     }
 
